be2fa0b11bcd1f077abdddaef1634a109c632cae
dong <ezreal.dong@stey.com>
2020-09-25 11:20:05 +0800

    展示客户的详情

--- pages/client/client_detail.vue
+++ pages/client/client_detail.vue
@@ -10,6 +10,17 @@
 		<text class="sub-title">签约点位: {{ client.signupPoint }}</text>
 		<text class="sub-title">税源地: {{ clientCompanyNames }}</text>
 		<text class="sub-title">渠道: {{ client.channel.channelName }}</text>
+		<text class="sub-title">联系人: {{ client.contactInformation.name }}</text>
+		<text class="sub-title">联系人电话: {{ client.contactInformation.phone }}</text>
+		<text class="sub-title">联系人地址: {{ client.contactInformation.address}}</text>
+		<text class="sub-title">开票公司: {{ client.invoiceInfo.companyName}}</text>
+		<text class="sub-title">纳税识别号: {{ client.invoiceInfo.taxNumber}}</text>
+		<text class="sub-title">公司注册地址: {{ client.invoiceInfo.registerAddress}}</text>
+		<text class="sub-title">开票联系方式: {{ client.invoiceInfo.phone}}</text>
+		<text class="sub-title">开户行: {{ client.invoiceInfo.bankName}}</text>
+		<text class="sub-title">银行卡号: {{ client.invoiceInfo.bankNumber}}</text>
+		<text class="sub-title">邮寄地址: {{ client.invoiceInfo.postAddress}}</text>
+		<text class="sub-title">签约时间: {{ client.signupTime}}</text>
 		<text class="sub-title">备注: {{ client.mark.length > 0 ? client.mark : '无' }}</text>
 	</view>
 </template>
@@ -33,7 +44,13 @@
 			}
 		},
 		computed: {
-			...mapGetters(['clientList'])
+			...mapGetters(['clientList']),
+			clientCompanyNames() {
+				if (this.client) {
+					return this.client.companyList.map(company => company.companyName).toString()
+				}
+				return ''
+			}
 		},
 		methods: {
 			onClickDetail() {
--- pages/my/my.vue
+++ pages/my/my.vue
@@ -1,6 +1,9 @@
 <template>
 	<view>
-		
+		<view>部分已知问题(红色是未解决)</view>
+		<view style="color: red;">1. 客户中的渠道和税源地应该是可更新的,现在是选择后就直接写死,一旦编辑了税源地或者渠道,对应的客户的不会更新</view>
+		<view style="color: red;">2. 客户详情的UI页面需要优化</view>
+		<view style="color: red;">3. 客户,渠道,税源地的列表展示的信息布局需要优化下</view>
 	</view>
 </template>
 
